4ea61cf9ca12510e4673ddf6d375553d0cdcebb3
Darcy Jones <darcy.ab.jones@gmail.com>
2018-08-19 23:31:31 +0800

    basic implementation gc and cri

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,67 @@ extern crate bio;
 
 pub mod freqs;
 
+pub mod gc {
+    use bio::io::fasta;
+    use freqs::Frequencies;
+
+    pub fn gc_content(seq: &fasta::Record) -> f64 {
+
+        let mut freq = Frequencies::new(6);
+        freq.extend(seq.seq());
+
+        let gc = (freq.count(&&b'G') + freq.count(&&b'C')) as f64;
+        let len = seq.seq().len() as f64;
+
+        gc / len
+    }
+}
+
+pub mod cri {
+    use freqs::Frequencies;
+
+    pub fn cri(seq: &[u8]) -> f64 {
+        let dinucs = seq.windows(2).map(|x| [x[0], x[1]]);
+
+        let mut di_freq = Frequencies::new(25);
+        di_freq.extend(dinucs);
+
+        let ta = di_freq.count(b"TA") as f64;
+        let at = di_freq.count(b"AT") as f64;
+
+        let ca = di_freq.count(b"CA") as f64;
+        let ac = di_freq.count(b"AC") as f64;
+
+        let gt = di_freq.count(b"GT") as f64;
+        let tg = di_freq.count(b"TG") as f64;
+
+        let num = ca + tg;
+        let denom = ac + gt;
+        let ratio = if denom == 0.0 {
+            0.0
+        } else {
+            num / denom
+        };
+
+        let offset = ta / at;
+        offset - ratio
+    }
+}
+
 pub mod runner {
 
     use std::path::Path;
-    use freqs::Frequencies;
     use bio::io::fasta;
+    use cri;
 
     pub fn run(path: &Path) {
 
-        let mut reader = fasta::Reader::from_file(path).unwrap();
-        let mut record = fasta::Record::new();
+        let reader = fasta::Reader::from_file(path).unwrap();
 
-        reader.read(&mut record);
+        for record in reader.records() {
+            let frac = cri::cri(&record.unwrap().seq()); 
+            println!("{}", frac);
+        }
 
-        let mut freq = Frequencies::new(2);
-        freq.extend(record.seq().into_iter());
-        println!("{:#?}", freq);
-
-        println!("{}", freq.count(&&b'A'));
     }
 }
