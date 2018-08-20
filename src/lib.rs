302a6f103c500bd50cc3f3e869288c8b23c8757b
Darcy Jones <darcy.ab.jones@gmail.com>
2018-08-20 17:08:41 +0800

    basic vcf reader kind of... no logic for snps yet.

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,31 @@ pub mod freqs;
 pub mod stats;
 
 pub mod snp {
+    use bio::io::fasta;
     use rust_htslib::bcf;
+    use rust_htslib::bcf::Read;
+    use rust_htslib::bcf::Record;
+    use std::collections::HashMap;
+    use std::str;
+    use std::fs::File;
 
+    pub fn get_samples(reader: &bcf::Reader) -> Vec<&str> {
+        reader.header()
+            .samples()
+            .iter()
+            .map(|x| str::from_utf8(x).unwrap())
+            .collect()
+    }
 
+    pub fn fasta_to_dict(records: fasta::Reader<File>) -> HashMap<String, fasta::Record> {
+        let mut output = HashMap::new();
+
+        for rec in records.records() {
+            let rec2 = &rec.unwrap();
+            output.insert(rec2.id().to_owned(), rec2.to_owned());
+        }
+        output
+    }
 }
 
 pub mod runner {
@@ -18,7 +40,7 @@ pub mod runner {
     use rust_htslib::bcf::Read;
     use rust_htslib::bcf::Record;
     use stats;
-    use std::str;
+    use snp;
 
     pub fn run_gc(path: &Path, size: usize, step: usize) {
 
@@ -26,7 +48,7 @@ pub mod runner {
 
         for record in reader.records() {
             let rec = record.unwrap();
-            let frac = stats::run_sliding_windows(&rec, size, step, stats::gc_content); 
+            let frac = stats::run_sliding_windows(&rec, size, step, stats::gc_content);
             for (seqid, start, end, score) in frac {
                 println!("{}\t{}\t{}\t{}", seqid, start, end, score);
             }
@@ -40,7 +62,7 @@ pub mod runner {
 
         for record in reader.records() {
             let rec = record.unwrap();
-            let frac = stats::run_sliding_windows(&rec, size, step, stats::cri); 
+            let frac = stats::run_sliding_windows(&rec, size, step, stats::cri);
             for (seqid, start, end, score) in frac {
                 println!("{}\t{}\t{}\t{}", seqid, start, end, score);
             }
@@ -52,8 +74,6 @@ pub mod runner {
         let freader = fasta::Reader::from_file(fasta).unwrap();
         let mut breader = bcf::Reader::from_path(vcf).unwrap();
 
-        let samples: Vec<&str> = breader.header().samples().iter().map(|x| str::from_utf8(x).unwrap()).cloned().collect();
-
         for record in breader.records() {
             let mut this = record.unwrap();
             println!("id {:?}", this.id());
@@ -63,5 +83,9 @@ pub mod runner {
             println!("geno 1 {:?}", this.genotypes().unwrap().get(2));
             break;
         }
+        let samples = snp::get_samples(&breader);
+        println!("{:?}", samples);
+        let genome = snp::fasta_to_dict(freader);
+        println!("{:?}", genome);
     }
 }
